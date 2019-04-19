1035384c742260f5edb82892c0bc4373e4ce98b8
Andrew Brillaud <drewb3@bu.edu>
2019-04-18 21:10:48 -0400

    Fixed?

--- app/src/main/java/com/example/Jesture/Adapter.java
+++ app/src/main/java/com/example/Jesture/Adapter.java
@@ -1,4 +1,4 @@
-package com.example.animatedbackground;
+package com.example.Jesture;
 
 import android.content.Context;
 import android.support.annotation.NonNull;
@@ -8,6 +8,8 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.example.Jesture.R;
 
 import java.util.List;
 
